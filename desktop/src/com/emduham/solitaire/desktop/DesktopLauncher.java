4471c4f9ea9ccad594f3ede9f8cc30d6c7fd9dc4
Evan M. Duhamel <evanduhamel@gmail.com>
2016-12-23 16:23:38 -0500

    initial commit

---
+++ desktop/src/com/emduham/solitaire/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.emduham.solitaire.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.emduham.solitaire.SolitaireApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SolitaireApp(), config);
+	}
+}
