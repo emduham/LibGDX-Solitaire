75d90cbbf500ded39cd3c1b1dccbe5f663307faa
Evan M. Duhamel <evanduhamel@gmail.com>
2017-01-16 20:42:59 -0500

    add icons

--- desktop/src/com/emduham/solitaire/desktop/DesktopLauncher.java
+++ desktop/src/com/emduham/solitaire/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.emduham.solitaire.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.emduham.solitaire.SolitaireApp;
@@ -11,6 +12,9 @@ class DesktopLauncher {
         cfg.width = (int) (LwjglApplicationConfiguration.getDesktopDisplayMode().width * 0.8);
         cfg.height = cfg.width / 16 * 9;
         cfg.resizable = true;
+        cfg.addIcon("icon128.png", Files.FileType.Internal);
+        cfg.addIcon("icon32.png", Files.FileType.Internal);
+        cfg.addIcon("icon16.png", Files.FileType.Internal);
         new LwjglApplication(new SolitaireApp(), cfg);
     }
 }
